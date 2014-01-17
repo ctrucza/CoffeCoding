347c5d57be8debf277f9160e2d42982509061820
Csaba Trucza <csaba.trucza@iquestgroup.com>
2014-01-17 09:04:25 +0200

    Coffee coding day 2

--- StringCalculator/StringCalculatorTests.cs
+++ StringCalculator/StringCalculatorTests.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
@@ -32,39 +31,103 @@ namespace StringCalculator
             Assert.AreEqual(expected, Add(numbers));
         }
 
+        [Test]
+        public void Add_NewLine_TreatedAsSeparator()
+        {
+            Assert.AreEqual(3,Add("1\n2"));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_TwoSeparatorsNextToEachOther_IsError()
+        {
+            Add("1\n,2");
+        }
+
+        [Test]
+        public void Add_WithCustomDelimiter_UsesTheCustomDelimiter()
+        {
+            Assert.AreEqual(3, Add("//%\n1%2"));
+        }
+
         private int Add(string numbers)
         {
+            if (numbers == "")
+                return 0;
             var bunchOfIntegers = ParseNumbers(numbers);
             return bunchOfIntegers.Sum();
         }
 
         private IEnumerable<int> ParseNumbers(string numbers)
         {
-            var bunchOfNumbers = SplitNumbers(numbers);
-            var bunchOfIntegers = TransformNumbers(bunchOfNumbers);
-            return bunchOfIntegers;
+
+            var lines = SplitNumbersIntoLines(numbers).ToList();
+
+            char delimiter = GetDelimiter(lines);
+            
+            return GetNumberLines(lines, delimiter);
         }
 
-        private IEnumerable<int> TransformNumbers(IEnumerable<string> bunchOfNumbers)
+        private IEnumerable<int> GetNumberLines(IEnumerable<string> lines, char delimiter)
         {
             List<int> result = new List<int>();
-            foreach (string aNumber in bunchOfNumbers)
+            var operationLines = lines.Where(line => !line.StartsWith("//"));
+            foreach (var line in operationLines)
             {
-                result.Add(NumberAsInt(aNumber));
+                result.AddRange(GetNumbersInLine(line, delimiter));
             }
             return result;
         }
 
-        private int NumberAsInt(string aNumber)
+        private char GetDelimiter(IEnumerable<string> lines)
         {
-            if (aNumber == "")
-                return 0;
-            return Int32.Parse(aNumber);
+            char delimiter = ',';
+
+            var delimiterLine = lines.SingleOrDefault(line => line.StartsWith("//"));
+            if (delimiterLine != null)
+                delimiter = GetDelimiterFromLine(delimiterLine);
+
+            return delimiter;
         }
 
-        private IEnumerable<string> SplitNumbers(string numbers)
+        private char GetDelimiterFromLine(string delimiterLine)
         {
-            return numbers.Split(',');
+            return delimiterLine.Substring(2,1)[0];
+        }
+
+        private IEnumerable<int> GetNumbersInLine(string line, char delimiter)
+        {
+            var bunchOfNumbers = SplitNumbers(line, delimiter);
+            if (bunchOfNumbers.Any(number => number == ""))
+                throw new ArgumentException(line);
+
+            var bunchOfIntegers = TransformNumbers(bunchOfNumbers);
+            return bunchOfIntegers;
+        }
+
+        private IEnumerable<string> SplitNumbersIntoLines(string numbers)
+        {
+            return numbers.Split('\n');
+        }
+
+        private IEnumerable<int> TransformNumbers(IEnumerable<string> bunchOfNumbers)
+        {
+            var result = new List<int>();
+            foreach (string number in bunchOfNumbers)
+            {
+                result.Add(NumberAsInt(number));
+            }
+            return result;
+        }
+
+        private int NumberAsInt(string number)
+        {
+            return Int32.Parse(number);
+        }
+
+        private IEnumerable<string> SplitNumbers(string numbers, char delimiter)
+        {
+            return numbers.Split(delimiter);
         }
     }
 }
